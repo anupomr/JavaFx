54b539dc1e6f8b2df7520fb87832f912f0c6481c
Anupom Roy <anupomr@yahoo.com>
2017-11-16 16:03:02 -0500

    Done

--- L14TreeView/src/application/MainController.java
+++ L14TreeView/src/application/MainController.java
@@ -8,24 +8,34 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class MainController implements Initializable 
 {
 	@FXML
 	TreeView <String> treeView;
+	Image icon=new Image(getClass().getResourceAsStream("/img/icon.png"));
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) 
 	{
-		TreeItem<String> root= new TreeItem<>("Root");
+		TreeItem<String> root= new TreeItem<>("Root", new ImageView(icon));
 		
 		TreeItem<String> noodA= new TreeItem<>("nood A");
 		TreeItem<String> noodB= new TreeItem<>("nood B");
 		TreeItem<String> noodC= new TreeItem<>("nood C");
+		
 		root.getChildren().addAll(noodA,noodB,noodC);
+		
 		/*root.getChildren().add(noodA);
 		root.getChildren().add(noodB);
 		root.getChildren().add(noodC);*/
+		
+		TreeItem<String> noodA1= new TreeItem<>("nood A 1");
+		TreeItem<String> noodB1= new TreeItem<>("nood B 1");
+		TreeItem<String> noodC1= new TreeItem<>("nood C 1");
+		noodA.getChildren().addAll(noodA1,noodB1,noodC1);
 		
 		treeView.setRoot(root);
 	}
