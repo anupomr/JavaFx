9d83db276a412ffee9b212932ff8f26db5d340b2
Anupom Roy <anupomr@yahoo.com>
2017-11-17 20:09:12 -0500

    Added File Chooser Functionality

---
+++ L16FileChooser/src/application/MainController.java
@@ -0,0 +1,64 @@
+package application;
+
+import java.io.File;
+import java.util.List;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+
+public class MainController 
+{
+	@FXML
+	private Button btn1;
+	
+	@FXML
+	private Button btn2;
+
+	
+	@FXML
+	private ListView listView;
+	
+	public void Button1Action(ActionEvent event) 
+	{
+		FileChooser fc=new FileChooser();
+		fc.setInitialDirectory(new File("C:\\Users\\anupo\\Google Drive\\Semester 3\\COMP 228 003 JAVA"));
+		fc.getExtensionFilters().addAll(new ExtensionFilter("PDF Files" , "*.pdf"));
+		File selectedFile=fc.showOpenDialog(null);
+		
+		if(selectedFile != null) 
+		{
+			listView.getItems().add(selectedFile.getAbsolutePath());
+		}
+		else 
+		{
+			System.out.println("File is not valid");
+		}
+	}
+
+	public void Button2Action(ActionEvent event) 
+	{
+		FileChooser fc=new FileChooser();
+		fc.setInitialDirectory(new File("C:\\Users\\anupo\\Google Drive\\Semester 3\\COMP 228 003 JAVA"));
+		fc.getExtensionFilters().addAll(new ExtensionFilter("PDF Files" , "*.pdf"));
+		List<File> selectedFiles=fc.showOpenMultipleDialog(null);
+		
+		if(selectedFiles != null) 
+		{
+			for (int i = 0; i < selectedFiles.size(); i++) 
+			{
+				listView.getItems().add(selectedFiles.get(i).getAbsolutePath());	
+			}
+			
+		}
+		else 
+		{
+			System.out.println("File is not valid");
+		}
+	}
+
+
+}
